3046fd5131bc1d76bfd70a2e3d0402c7729f787c
eklipsorz <eklipsorz@protonmail.ch>
2022-01-03 20:24:39 +0800

    feat: add main layout

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 
+
+const methodOverride = require('method-override')
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -13,11 +16,27 @@ const port = process.env.PORT || 3000
 // define an application object
 const app = express()
 
+// settings for view engine 
+app.engine(".hbs", exphbs({
+  extname: ".hbs",
+  layoutsDir: process.cwd() + "/views/layouts",
+  defaultLayout: "main",
+  partialDir: "views/partials",
+}))
+
+app.set("view engine", ".hbs")
+
+// define a path for all static files
+app.use(express.static("public"))
+
+
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 
 // define application layer routes
 app.use('/', (req, res) => {
-  console.log('test project')
+  res.render('index')
 })
 
 // start to listen at 3000
