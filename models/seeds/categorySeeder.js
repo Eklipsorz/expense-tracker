f5f87ec0c84b5816875e3c43141d70af3b1bf96f
eklipsorz <eklipsorz@protonmail.ch>
2022-01-08 23:25:45 +0800

    refactor: recordSeeder.js

--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -10,20 +10,27 @@ const categories = require('./category.json').results
 
 db.on('open', async () => {
 
-  // check whether all seed category data have been built
-  // if true, it means they are built in database
-  // if false, it means they are not built
-  const hasCategories = (await categoryModel.find({}).lean().then()).length
 
-  // all seed category data are not built
-  if (!hasCategories) {
-    // create all seed category data into database
-    await categoryModel.create(categories)
-  }
-  
-  console.log('All type of categories have been built')
-  // close database connection
-  db.close()
+  // create each category according to category.json
+  const requests = categories.map((category) => {
+    const { name } = category
+
+    return categoryModel.findOne({ name })
+      .lean()
+      .then(async (targetCategory) => {
+        if (targetCategory) return
+        return await categoryModel.create(category)
+      })
+      .catch(error => console.log(error))
+  })
+
+  Promise.all(requests)
+    .then(() => {
+      console.log('All type of categories have been built')
+      db.close()
+    })
+
+
 
 })
 
